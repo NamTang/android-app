5cf7bbb9f669ad9b05ffc551daf20232e7f029da
NamTang <namkty96@gmail.com>
2019-05-23 14:10:08 +0700

    update

--- app/src/main/java/com/example/manager/LoginActivity.java
+++ app/src/main/java/com/example/manager/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.R;
 import com.example.model.Account;
 import com.example.service.RestService;
 import com.example.util.AESCrypt;
