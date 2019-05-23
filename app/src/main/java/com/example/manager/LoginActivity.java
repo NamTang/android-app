300bfae338134b8a4dde79e241dc2d93c1411cd1
NamTang <namkty96@gmail.com>
2019-05-23 15:43:17 +0700

    add text view error;

--- app/src/main/java/com/example/manager/LoginActivity.java
+++ app/src/main/java/com/example/manager/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.R;
@@ -20,8 +21,10 @@ import java.util.concurrent.ExecutionException;
 public class LoginActivity extends AppCompatActivity {
 
     EditText edtUserName, edtPassword;
+    TextView txtError;
     Gson gson = new Gson();
     AlertDialog alert;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
     private void addControls() {
         edtUserName = findViewById(R.id.edtUserName);
         edtPassword = findViewById(R.id.edtPassword);
+        txtError = findViewById(R.id.txtError);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setView(getLayoutInflater().inflate(R.layout.layout_loading_dialog, null));
         builder.setCancelable(false);
@@ -44,7 +48,7 @@ public class LoginActivity extends AppCompatActivity {
         String userName = edtUserName.getText().toString();
         String password = edtPassword.getText().toString();
         if ((userName == null || userName.equals("")) || (password == null || password.equals(""))) {
-            alert.dismiss();
+            txtError.setText(R.string.login_missing_value);
             Toast.makeText(this, R.string.login_missing_value, Toast.LENGTH_SHORT).show();
         } else {
             password = AESCrypt.encrypt(password);
@@ -66,14 +70,13 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
             } else if (result == null) {
-//                alert.dismiss();
+                txtError.setText(R.string.error);
                 Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show();
             } else {
-//                alert.dismiss();
+                txtError.setText(result);
                 Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
             }
-
-            alert.dismiss();
         }
+        alert.dismiss();
     }
 }
